931ef6777f5cf9a2d9c35f9bcf388589b5332392
a.ukadan <a.ukadan@arbuz.kz>
2024-01-04 01:58:08 +0500

    start project chatApp

---
+++ ChatApp/Controllers/ConversationsViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
